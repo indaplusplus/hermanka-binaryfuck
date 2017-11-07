67dc610e9d5869a281381d162d07aad567dfa24b
Herkarl <herkarlsson@gmail.com>
2017-11-07 18:10:25 +0100

    added commandline arguments

--- Interpreter/exceptions.h
+++ Interpreter/exceptions.h
@@ -20,4 +20,10 @@ public:
   runtime_error(std::string message = "") : error_message(message) {}
 };
 
+class argument_error : std::exception {
+public:
+  std::string error_message;
+  argument_error(std::string message = "") : error_message(message) {}
+};
+
 #endif //INTERPRETER_EXCEPTIONS_H
--- Interpreter/main.cpp
+++ Interpreter/main.cpp
@@ -9,17 +9,48 @@
 
 //#define DEBUG
 
-int main(int argc, char** argv) {
+std::string source_file = "";
+int source_type = 2;
+int mem_size = 256;
 
-  if (argc != 2) {
+void parse_args(int argc, char** argv) {
+  for (int i = 1; i < argc; ++i) {
+    std::string arg = argv[i];
+    if (arg[0] == '-') {
+      if (arg == "--bin" || arg == "-2") source_type = 2;
+      if (arg == "--oct" || arg == "-8") source_type = 8;
+      if (arg == "--hex" || arg == "-16") source_type = 16;
+      if (arg == "--b32" || arg == "-32") source_type = 32;
+      if (arg == "--memory" || arg == "-m") {
+        if (i+1 == argc)
+          throw argument_error("expected integer argument after -m flag");
+        arg = argv[++i];
+        try {
+          mem_size = std::stoi(arg);
+        } catch (std::exception) {
+          throw argument_error("expected integer argument after -m flag");
+        }
+      }
+    } else {
+      if (source_file != "")
+        throw argument_error("too many source files specified");
+      source_file = arg;
+    }
+  }
+  if (source_file == "") throw argument_error("No source file specified");
+}
+
+int main(int argc, char** argv) {
+  try {
+    parse_args(argc, argv);
+  } catch (argument_error e) {
+    std::cerr << "Error while parsing arguments: " << e.error_message << std::endl;
     exit(1);
   }
 
-  std::ifstream file;
-  try {
-    file.open(argv[1]);
-  } catch (std::exception e) {
-    std::cerr << "did not find file " << argv[1] << std::endl;
+  std::ifstream file(source_file.c_str());
+  if (!file.good()) {
+    std::cerr << "did not find file: " << source_file << std::endl;
     exit(1);
   }
 
@@ -29,7 +60,10 @@ int main(int argc, char** argv) {
   std::vector<Instruction> program;
 
   try {
-    program = parser.parse_bin(source);
+    if (source_type == 2) program = parser.parse_bin(source);
+    if (source_type == 8) program = parser.parse_oct(source);
+    if (source_type == 16) program = parser.parse_hex(source);
+    if (source_type == 32) program = parser.parse_32(source);
   } catch (syntax_error e) {
     std::cerr << "Syntax error: " << e.error_message << std::endl;
     exit(1);
@@ -40,7 +74,7 @@ int main(int argc, char** argv) {
   std::cerr << std::endl;
 #endif
 
-  Runner runner(program);
+  Runner runner(program, mem_size);
   try {
     runner.run();
   } catch (runtime_error e) {
